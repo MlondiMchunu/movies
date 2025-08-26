f98b1859a97bb7bcd8a13583b3d30dbbc950b115
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-26 12:52:32 +0200

    declare app version number and config settings struct

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello world")
+	//declare string containing application version number
+	const version = "1.0.0"
+
+	//define config struct to hold config settings
+	type config struct {
+		port int
+		env  string
+	}
+
 }
