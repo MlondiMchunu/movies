4b7d96e474d5a740af319626a95ebe221e248966
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-26 13:03:59 +0200

    read value of port and env command-line flags into the config struct

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,30 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+)
 
-//define config struct to hold config settings
+// declare string containing application version number
+const version = "1.0.0"
+
+// define config struct to hold config settings
 type config struct {
 	port int
 	env  string
 }
 
-//define struct to hold dependencies for http handler, helpers, and middleware
+// define struct to hold dependencies for http handler, helpers, and middleware
 type application struct {
 	config config
 	logger *log.Logger
 }
 
 func main() {
-	//declare string containing application version number
-	const version = "1.0.0"
+	//config struct instance
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.Parse()
 }
