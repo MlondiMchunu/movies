06fbc57595e438b6818879e5b668b51163950a03
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-26 12:57:27 +0200

    refactor main.go file: structs should be outside main func

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,20 +2,19 @@ package main
 
 import "log"
 
+//define config struct to hold config settings
+type config struct {
+	port int
+	env  string
+}
+
+//define struct to hold dependencies for http handler, helpers, and middleware
+type application struct {
+	config config
+	logger *log.Logger
+}
+
 func main() {
 	//declare string containing application version number
 	const version = "1.0.0"
-
-	//define config struct to hold config settings
-	type config struct {
-		port int
-		env  string
-	}
-
-	//define struct to hold dependencies for http handler, helpers, and middleware
-	type application struct {
-		config config
-		logger *log.Logger
-	}
-
 }
