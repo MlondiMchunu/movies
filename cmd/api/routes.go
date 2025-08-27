46f5016537c4c0089e4191fc555065da87d4260d
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-27 12:49:58 +0200

    migrate API routing from main.go to routes.go

---
+++ cmd/api/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	//initialize a new httprouter router instance
+	router := httprouter.New()
+
+	//register relevant methods, URL patterns and handler functions
+	router.Handler(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
+
+	return router
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module movies.mlo_dev.net
 
 go 1.24.5
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
