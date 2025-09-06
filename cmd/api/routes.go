125571fa88296513b0a351e1ecacc7f5158c3caa
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-06 20:23:45 +0200

    add id parameter in shouwMovieHandler route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,7 +13,7 @@ func (app *application) routes() *httprouter.Router {
 	//register relevant methods, URL patterns and handler functions
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.showMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies:id", app.showMovieHandler)
 
 	return router
 }
