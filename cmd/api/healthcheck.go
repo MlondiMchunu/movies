c10fc278660a11a9beffd5cd526fd9870988c3b8
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-12 22:54:11 +0200

    pass map to json.Marshal() function

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(res http.ResponseWriter, req *http.Request) {
+
+	//js := `{"status":"available","environment":%q,"version":%q}`
+	//js = fmt.Sprintf(js, app.config.env, version)
+
 	data := map[string]string{
 		"status":      "available",
 		"environment": app.config.env,
 		"version":     version,
 	}
 
-	js := `{"status":"available","environment":%q,"version":%q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(res, "Server encounteed a problem, could not pass request", http.StatusInternalServerError)
+		return
+	}
 
 	res.Header().Set("Content-Type", "application/json")
 
