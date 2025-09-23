0a52e808fea1206dbcc142374a5c5f04284f259a
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-23 13:59:29 +0200

    declare envelope map in healthCheckHandler method

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -9,13 +9,15 @@ func (app *application) healthCheckHandler(res http.ResponseWriter, req *http.Re
 	//js := `{"status":"available","environment":%q,"version":%q}`
 	//js = fmt.Sprintf(js, app.config.env, version)
 
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
-	err := app.writeJSON(res, http.StatusOK, data, nil)
+	err := app.writeJSON(res, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(res, "Server encountered a problem, could not pass request", http.StatusInternalServerError)
