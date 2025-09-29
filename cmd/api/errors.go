a1b740d3667ae77790105c831c012bb58eb20264
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-29 21:06:17 +0200

    declare errorResponse() method for sending json formatted error messages

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,3 +5,7 @@ import "net/http"
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.Println(err)
 }
+
+func (app *application) errorResponse(res http.ResponseWriter, req *http.Request, status int, message interface{}) {
+	env := envelope{"error": message}
+}
