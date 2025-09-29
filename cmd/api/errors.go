deda09e7cf3dd3c58c4b0bb674f5fce86eb6b329
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-29 21:09:17 +0200

    write response using writeJSON() helper

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -8,4 +8,10 @@ func (app *application) logError(r *http.Request, err error) {
 
 func (app *application) errorResponse(res http.ResponseWriter, req *http.Request, status int, message interface{}) {
 	env := envelope{"error": message}
+
+	err := app.writeJSON(res, status, env, nil)
+	if err != nil {
+		app.logError(req, err)
+		res.WriteHeader(500)
+	}
 }
