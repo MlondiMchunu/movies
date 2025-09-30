f3280867a0e1317c656a3d06b990c390fdffa01c
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-09-30 10:35:14 +0200

    convert the notFoundResponse() helper to a http.Handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,10 @@ func (app *application) routes() *httprouter.Router {
 	//initialize a new httprouter router instance
 	router := httprouter.New()
 
+	//convert the notFoundResponse() helper to a http.Handler
+	//using http.HandlerFunc() adapter
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
 	//register relevant methods, URL patterns and handler functions
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
