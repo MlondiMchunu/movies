c81054da3be8442adbd182871674fa9d17ff7e1f
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-10-07 21:55:45 +0200

    create a validation package and declare a regex for email validation

---
+++ internal/validator/validator.go
@@ -0,0 +1,9 @@
+package validator
+
+import "regexp"
+
+//declare a regular regular expression for checking email address format
+
+var (
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
