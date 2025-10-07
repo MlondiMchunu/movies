642a505d284bc314c05272fac85647e6eab303b9
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-10-07 15:02:00 +0200

    define error to be returned by UnmarshalJSON() if JSON string isn't parsed or converted

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,14 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 )
+
+// define an error that UnmarshalJSON() method can return
+// if JSON string cant be converted or parsed
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
