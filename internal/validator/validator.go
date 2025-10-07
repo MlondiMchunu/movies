c693ef103bd9f0217f49b12f777bee23de97ec25
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-10-07 23:23:30 +0200

    complete validation methods

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -15,3 +15,47 @@ type Validator struct {
 func New() *Validator {
 	return &Validator{Errors: make(map[string]string)}
 }
+
+//valid returns true if errors map doesnt contain any entries
+
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+// AddError adds an error message to the map
+func (v *Validator) AddError(key, message string) {
+	if __, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
+	}
+}
+
+// Check adds an error message to the map if vlidation check is not ok
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+// In returns true if a specific value is in a list of strings
+func In(value string, list ...string) bool {
+	for i := range list {
+		if value == list[i] {
+			return true
+		}
+	}
+	return false
+}
+
+// Matches function returns true if a string value matches a specific regex pattern
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+func Unique(values []string) bool {
+	uniqueValues := make(map[string]bool)
+
+	for __, value := range values {
+		uniqueValues[value] = true
+	}
+	return len(values) == len(uniqueValues)
+}
