f86c887600853521914fbaeaedfc1c584011340b
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-10-12 13:42:36 +0200

    execute validation checks for year

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -32,6 +32,10 @@ func (app *application) createMovieHandler(res http.ResponseWriter, req *http.Re
 	v.Check(input.Title != "", "title", "must be provided!")
 	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
 
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
 	fmt.Fprintf(res, "%+v\n", input)
 
 }
